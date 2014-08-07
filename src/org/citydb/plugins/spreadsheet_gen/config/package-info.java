2724c2075f7af9dfcbaa91d3930dc9fd65f9ac5a
yaozhihang <zhihang.yao@tum.de>
2014-08-07 11:32:09 +0200

    changed XML namespace

--- src/org/citydb/plugins/spreadsheet_gen/config/package-info.java
+++ src/org/citydb/plugins/spreadsheet_gen/config/package-info.java
@@ -1,5 +1,5 @@
 @javax.xml.bind.annotation.XmlAccessorType(javax.xml.bind.annotation.XmlAccessType.FIELD)
-@javax.xml.bind.annotation.XmlSchema(namespace="http://www.gis.tu-berlin.de/3dcitydb-impexp/config/SPSHG", 
-		xmlns = { @javax.xml.bind.annotation.XmlNs(prefix = "spshg", namespaceURI = "http://www.gis.tu-berlin.de/3dcitydb-impexp/config/spreadsheet_generator") },
+@javax.xml.bind.annotation.XmlSchema(namespace="http://www.3dcitydb.org/importer-exporter/config/spshg", 
+		xmlns = { @javax.xml.bind.annotation.XmlNs(prefix = "spshg", namespaceURI = "http://www.3dcitydb.org/importer-exporter/config/spshg") },
 		elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package org.citydb.plugins.spreadsheet_gen.config;
